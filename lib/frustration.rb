b3959ddd58d94fd91fcb26729ba082b5d0b3f8ae
Chris <ignatz@gmail.com>
2016-04-30 11:22:33 -0400

    init

---
+++ lib/frustration.rb
@@ -0,0 +1,8 @@
+require "frustration/version"
+require 'yamlable'
+
+module Frustration
+  class Frustrated
+    include Yamlable
+  end
+end
---
+++ lib/frustration/version.rb
@@ -0,0 +1,3 @@
+module Frustration
+  VERSION = "0.1.0"
+end
